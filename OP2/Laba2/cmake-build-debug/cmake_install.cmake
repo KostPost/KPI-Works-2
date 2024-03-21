6b7ca82f3e7046bd97cb63c5568f2adaa55c0a69
KostPost <kt5082206@gmail.com>
2024-03-21 22:01:20 +0200

    add laba 1

--- OP2/Laba2/cmake-build-debug/cmake_install.cmake
+++ OP2/Laba2/cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "P:/Proga/IDEs/CLion 2023.3.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/KostPost/AppData/Local/Programs/CLion Nova/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
